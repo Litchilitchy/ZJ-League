12fdda1d7d0bb00e71e043c10e6498a018b9b66d
SummerLitchichichy <657911141@qq.com>
2018-08-18 18:42:08 +0800

    Add files via upload

---
+++ extract_image.py
@@ -0,0 +1,34 @@
+from mxnet import nd, image
+import os
+
+
+from mxnet.gluon.model_zoo import vision
+from mxnet.gluon import nn
+import logging
+
+
+def load_image(img_path, long_side_length):
+    x = image.imread(img_path)
+    x = image.resize_short(x, long_side_length)
+    x, _ = image.center_crop(x, (224, 224))
+    return x
+
+
+def get_image_feature(img_path):
+    img_net = vision.inception_v3(pretrained=True)
+    img = load_image(img_path)
+
+    feature = img_net.features(img)
+    logging.debug("feature shape is %s", nd.shape(feature))
+
+    return feature
+
+
+def output_image_feature(data_path):
+    image_feature = []
+    for filename in os.listdir(data_path):
+        feature = get_image_feature(os.path.join(filename))
+        image_feature.append(feature)
+
+    # output feature to file
+    # 此处需要先看一下feature的形状，image_feature是feature的简单list集合
