2472943cfa9d9ad2c0684417d5e421fd564dad99
SummerLitchichichy <657911141@qq.com>
2018-09-20 14:21:02 +0800

    Add files via upload

--- data/get_video_frame.py
+++ data/get_video_frame.py
@@ -1,4 +1,9 @@
-# -*- coding: UTF-8 -*-
+import os
+import shutil
+from PIL import Image
+import matplotlib.pyplot as plt
+from queue import PriorityQueue as PQueue
+import cv2
 
 
 # 正则化图像
@@ -45,16 +50,7 @@ def calaHashSimilarity(img1, img2):
 
 
 #选出关键帧
-import os
-import shutil
-from PIL import Image
-import matplotlib.pyplot as plt
-from queue import PriorityQueue as PQueue
-
-#-----------------------------------------------路径修改一下
-#imgPath = "datalab/2193/data/tempImg/"
-outputPath = "datalab/2193/data/output/"
-
+'''
 def getKeyFrame(name):
   keyFrame = PQueue()
   threshold = -1
@@ -68,7 +64,8 @@ def getKeyFrame(name):
     else:
       img2 = Image.open(imgPath + file)
       sim = 0 - calaHashSimilarity(img1, img2)/64
-      if (sim > threshold):
+      print(sim)
+      if (sim >= threshold):
         if (keyFrame.qsize() == 3):
           keyFrame.get()
           threshold = sim
@@ -85,52 +82,56 @@ def getKeyFrame(name):
     
   for file in files:
     os.remove(imgPath + file)
-
-
+'''
+def getFrame(dataPath, outputPath):
 #每8帧提取图片，保存符合条件的3张在outputPath，命名格式：视频名称_1.jpg
 #为了命名整齐，此处视频默认没超过1000帧的
-import cv2
+  videos = os.listdir(dataPath)
 
-dataPath = 'train'
-videos = os.listdir(dataPath)
+  for v in videos:
+    name = v.split('.')[0]
+    print(v)
+    #每8帧提取图片
+    vc = cv2.VideoCapture(dataPath + '/' + v) #读入视频文件
+    rval = False
+    frame_num = 0
+    if vc.isOpened(): #判断是否正常打开
+        rval, frame = vc.read()
+        frame_num = vc.get(7)
+        print(frame_num)
+    else:
+        rval = False
+    
+    isVid = rval
+    count = 0
+    num = frame_num /  6
+    num = int(num) + 1
+    #提取5张关键帧
 
-if not os.path.exists(dataPath + '/tempImg'):
-  os.mkdir(dataPath + '/tempImg')
-if not os.path.exists(dataPath + '/output'):
-  os.mkdir(dataPath + '/output')
+    if isVid:
+      s = 0
+      while rval:
+        if rval and count % num == 0 and count != 0 and count != frame_num:
+          cv2.imwrite(outputPath + name + '_' + str(s) + '.jpg',frame)
+          s = s + 1
+          print("save " + name + "_" + str(count / num))
+        rval, frame = vc.read()
+        count = count + 1
+      vc.release()
+      #getKeyFrame(name)
+    
 
-#imgPath = 'datalab/2193/data/tempImg/'
-outputPath = 'datalab/2193/data/output/'
+if __name__ == "__main__":
+  testPath = './test_video'
+  trainPath ='./train_video'
 
-for v in videos:
-  name = v.split('.')[0]
-  print(v)
-  #每8帧提取图片
-  vc = cv2.VideoCapture(dataPath + '/' + v) #读入视频文件
-  c=1
-  rval = False
-  if vc.isOpened(): #判断是否正常打开
-      rval, frame = vc.read()
-  else:
-      rval = False
-  isVid = rval
-  timeF = 8  #视频帧计数间隔频率
-  while rval:   #循环读取视频帧
-      #print("-----"+str(c))
-      rows, cols, channel = frame.shape
-      frame=cv2.resize(frame,(224, 224),fx=0,fy=0,interpolation=cv2.INTER_AREA)
-      if(c%timeF == 0): #每隔timeF帧进行存储操作
-          print("--write-to-file---: " + str(c))
-          s = ""
-          if (c < 100):
-              s = s + "0"
-          if (c < 10):
-              s = s + "0"
-          cv2.imwrite(imgPath + name + '_' + s + str(c) + '.jpg',frame) #存储为图像在imgPath,视频名称_000.jpg
-      c = c + 1
-      rval, frame = vc.read()
-  vc.release()
-  
-  #提取3张关键帧
-  if isVid:
-    getKeyFrame(name)
+  if not os.path.exists(testPath + '/test_img'):
+    os.mkdir(testPath + '/test_img')
+  if not os.path.exists(trainPath + '/train_img'):
+    os.mkdir(trainPath + '/train_img')
+
+  testImgPath = './test_video/test_img/'
+  trainImgPath = './train_video/train_img/'
+
+  getFrame(testPath, testImgPath)
+  getFrame(trainPath, trainImgPath)
